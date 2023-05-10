58959c27548565bed2c66c614923c1d8a1f0d495
kg0r0 <kg0r0@yahoo.co.jp>
2023-05-10 23:28:58 +0900

    Imported passport-strategy

---
+++ src/strategy.ts
@@ -0,0 +1,7 @@
+import { Strategy as PassportStrategy } from "passport-strategy";
+
+export abstract class AbstractStrategy extends PassportStrategy {
+}
+
+export class Strategy extends AbstractStrategy {
+}
