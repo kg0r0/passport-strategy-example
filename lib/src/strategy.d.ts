0f42b5ada78b85f3101ac442866930f677b25c15
kg0r0 <kg0r0@yahoo.co.jp>
2023-05-11 00:49:07 +0900

    Add Augmented Methods

--- lib/src/strategy.d.ts
+++ lib/src/strategy.d.ts
@@ -1,5 +1,15 @@
-import { Strategy as PassportStrategy } from "passport-strategy";
+import { Strategy as PassportStrategy } from 'passport-strategy';
+import { Request } from 'express';
+export interface AuthenticateOptions {
+}
 export declare abstract class AbstractStrategy extends PassportStrategy {
 }
 export declare class Strategy extends AbstractStrategy {
+    authenticate(req: Request, options: AuthenticateOptions): void;
+    success(user: any, info?: any): void;
+    fail(challenge: any, status: number): void;
+    fail(status: number): void;
+    redirect(url: string, status?: number | undefined): void;
+    pass(): void;
+    error(err: Error): void;
 }
--- src/strategy.ts
+++ src/strategy.ts
@@ -1,5 +1,5 @@
-import { Strategy as PassportStrategy } from "passport-strategy";
-import { Request, Response } from 'express';
+import { Strategy as PassportStrategy } from 'passport-strategy';
+import { Request } from 'express';
 
 export interface AuthenticateOptions {
 }
@@ -8,6 +8,29 @@ export abstract class AbstractStrategy extends PassportStrategy {
 }
 
 export class Strategy extends AbstractStrategy {
+
   authenticate(req: Request, options: AuthenticateOptions): void {
+  }
+
+  success(user: any, info?: any): void {
+
+  }
+
+  fail(challenge: any, status: number): void;
+  fail(status: number): void;
+  fail(challenge: unknown, status?: unknown): void {
+
+  }
+
+  redirect(url: string, status?: number | undefined): void {
+
+  }
+
+  pass(): void {
+
+  }
+
+  error(err: Error): void {
+
   }
 }
